0c97ef9a09f69f1edd0b326407b1990d7514c7ff
Phillip Nguyen <png9981@gmail.com>
2024-08-27 23:54:18 +0700

    feat: rearrange directory

--- cdk/cdk/code_pipeline.py
+++ cdk/cdk/code_pipeline.py
@@ -2,21 +2,28 @@ from constructs import Construct
 
 from aws_cdk import (
     Stack,
-    pipelines
+    pipelines,
+    Stage
 ) 
+
+class TestStageStack(Stack):
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+class TestStage(Stage):
+    def __init__(self, scope: Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+        TestStageStack(self, "TestStageStack")
 
 class PipelineStack(Stack):
 
     def __init__(self, scope: Construct, id: str, connection_arn: str,**kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        pipeline = pipelines.CodePipeline(self, "Pipeline",
-            synth=pipelines.ShellStep("Synth",
-                input=pipelines.CodePipelineSource.connection("saldyy/aws-code-pipeline-101",
-                                                              "master",
-                                                              connection_arn=connection_arn),
-                commands=["make build"]
-            )
-        )
+        pipeline = pipelines.CodePipeline(self, "Pipeline")
+        test_stage = TestStage(self, "Test")
+        pipeline.add_stage(test_stage, pre=[
+                pipelines.ManualApprovalStep("Approve")
+            ])
 
 
