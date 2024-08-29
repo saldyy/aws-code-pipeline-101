6d9e60618ec4bf3c457c14b1c0a3d80c53f0c42c
Phillip Nguyen <png9981@gmail.com>
2024-08-29 13:01:00 +0700

    feat: adding ecr with iam role

--- cdk/cdk/code_pipeline.py
+++ cdk/cdk/code_pipeline.py
@@ -1,3 +1,4 @@
+from aws_cdk.aws_iam import IRole
 from constructs import Construct
 
 from aws_cdk import (
@@ -10,7 +11,8 @@ from aws_cdk import (
 
 class PipelineStack(Stack):
 
-    def __init__(self, scope: Construct, id: str, connection_arn: str,**kwargs) -> None:
+    def __init__(self, scope: Construct, id: str, connection_arn: str, ecr_arn: str,
+                 ecr_iam_access_role_arn = IRole, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         pipeline = codepipeline.Pipeline(self, "Pipeline")
@@ -58,3 +60,32 @@ class PipelineStack(Stack):
         pipeline.add_stage(stage_name="Test", actions=[test_action])
 
 
+        build_project = codebuild.PipelineProject(
+            self, "GolangBuildProject",
+            role=ecr_iam_access_role_arn,
+            build_spec=codebuild.BuildSpec.from_object({
+                "version": "0.2",
+                "phases": {
+                    "build": {
+                        "commands": [
+                            "echo --------START BUILD--------",
+                            "cd app",
+                            "aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin {ecr_arn}".format(ecr_arn=ecr_arn),
+                            "docker build -t golang-proj-$CODEBUILD_BUILD_NUMBER .",
+                            "docker tag golang-repository:$CODEBUILD_BUILD_NUMBER {ecr_arn}:$CODEBUILD_BUILD_NUMBER".format(ecr_arn=ecr_arn),
+                            "docker push {ecr_arn}:$CODEBUILD_BUILD_NUMBER".format(ecr_arn=ecr_arn)
+                        ]
+                    }
+                }
+            }),
+            environment=codebuild.BuildEnvironment(
+                build_image=codebuild.LinuxBuildImage.STANDARD_5_0
+            )
+        )
+
+        build_action = codepipeline_actions.CodeBuildAction(
+            action_name="RunBuild",
+            project=build_project,
+            input=source_output
+        )
+        pipeline.add_stage(stage_name="Build", actions=[build_action])
