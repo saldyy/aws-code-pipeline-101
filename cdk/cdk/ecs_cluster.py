24d64cedf3b947bf0794848115f888be3bc8196c
Phillip Nguyen <png9981@gmail.com>
2024-08-31 14:49:02 +0700

    feat: adding ecs service and default task definition

--- cdk/cdk/ecs_cluster.py
+++ cdk/cdk/ecs_cluster.py
@@ -1,13 +1,32 @@
-from aws_cdk.aws_ec2 import IVpc
 from constructs import Construct
 
-from aws_cdk import Stack, aws_ecs as ecs
+from aws_cdk import (
+        Stack,
+        aws_ecs as ecs,
+        aws_ec2 as ec2
+)
 
 
 class EcsStack(Stack):
 
-    def __init__(self, scope: Construct, id: str, vpc: IVpc, **kwargs) -> None:
+    def __init__(self, scope: Construct, id: str, vpc: ec2.IVpc, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        # Create ECS cluster
-        ecs.Cluster(self, "MyCluster", vpc=vpc, cluster_name="GolangCluster")
+        self.cluster = ecs.Cluster(self, "MyCluster", vpc=vpc, cluster_name="GolangCluster")
+        # Add capacity to it
+        self.cluster.add_capacity("DefaultAutoScalingGroupCapacity",
+            instance_type=ec2.InstanceType("t2.small"),
+            desired_capacity=3
+        )
+
+        self.task_definition = ecs.Ec2TaskDefinition(self, "GolangTaskDefinition")
+
+        self.task_definition.add_container("DefaultContainer",
+            image=ecs.ContainerImage.from_registry("amazon/amazon-ecs-sample"),
+            memory_limit_mib=512
+        )
+
+        self.ecs_service = ecs.Ec2Service(self, "GolangServiceService",
+            cluster=self.cluster,
+            task_definition=self.task_definition
+        )
