79e675cc7113f9f653a0fe2a899ccd8f69235776
Phillip Nguyen <png9981@gmail.com>
2024-08-31 14:29:05 +0700

    feat: adding ecs cluster stack

--- cdk/app.py
+++ cdk/app.py
@@ -2,7 +2,7 @@
 
 import aws_cdk as cdk
 
-from cdk import codestar_connection
+from cdk import codestar_connection, ecs_cluster
 from cdk.code_pipeline import PipelineStack
 from cdk.vpc import VpcStack
 
@@ -10,6 +10,7 @@ app = cdk.App()
 codestar_connection = "arn:aws:codeconnections:ap-southeast-1:454159633175:connection/686db3a7-1624-424f-8b7f-76abfe10fc3f"
 
 vpc_stack = VpcStack(app, "Vpc-golang-cicd")
+ecs_cluster_stack = ecs_cluster.EcsStack(app, "Ecs-golang-cicd", vpc=vpc_stack.vpc)
 PipelineStack(app,
               "Pipeline-golang-cicd",
               connection_arn=codestar_connection,
